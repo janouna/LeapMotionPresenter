97cdd59f038f7931ef1c48281d615ceb6423fb46
James Anouna <jganouna@wpi.edu>
2014-11-03 18:13:05 -0500

    Added HandStateObservable

---
+++ src/edu/wpi/cs/lmp/leap/HandStateObservable.java
@@ -0,0 +1,21 @@
+package edu.wpi.cs.lmp.leap;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+public class HandStateObservable {
+	public final HandStateObservable INSTANCE = new HandStateObservable();
+	private ObjectProperty<HandState> handState = new SimpleObjectProperty<>();
+	
+	private HandStateObservable(){
+		handState.set(HandState.GONE);
+	}
+	
+	public void setHandState(HandState state){
+		handState.set(state);
+	}
+	
+	public ObjectProperty<HandState> getHandState() {
+		return handState;
+	}
+}
